0f74257ef71b43c0e7eecf6276ff62b53944feb3
felixonomine <onomineevuarhere@gmail.com>
2021-06-21 15:06:34 +0100

    my android studio

--- app/src/main/java/com/evuarhere/myapplication/MainActivity.kt
+++ app/src/main/java/com/evuarhere/myapplication/MainActivity.kt
@@ -2,8 +2,18 @@ package com.evuarhere.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    val userName = "admin"
+    val passWord = "admin"
+
+   private lateinit var  userNameInput:EditText
+   private lateinit var passWordInput:EditText
+   private lateinit var changeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
